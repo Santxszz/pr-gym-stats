d09d5f0ff9282450fd135141272c0c66f9386d64
Erek Couto Santos <erekcoutosantos22@gmail.com>
2025-04-02 10:38:30 -0300

    refactor: changed fields of schemas

--- src/database/migrations/0000_uneven_cargill.sql
+++ src/database/migrations/0000_uneven_cargill.sql
@@ -27,6 +27,29 @@ CREATE TABLE "treinos" (
 	"updated_at" timestamp DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
+CREATE TABLE "users_table" (
+	"id" serial NOT NULL,
+	"ext_id" text PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"nickname" text NOT NULL,
+	"email" text NOT NULL,
+	"height" integer NOT NULL,
+	"weight" integer NOT NULL,
+	"age" integer NOT NULL,
+	"zip_code" text NOT NULL,
+	"street" text NOT NULL,
+	"street_number" text NOT NULL,
+	"neighborhood" text NOT NULL,
+	"city" text NOT NULL,
+	"state" text NOT NULL,
+	"password" text NOT NULL,
+	"is_active" integer DEFAULT 1 NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	"deleted_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "users_table_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
 ALTER TABLE "equipamentos" ADD CONSTRAINT "equipamentos_usuario_id_users_table_ext_id_fk" FOREIGN KEY ("usuario_id") REFERENCES "public"."users_table"("ext_id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "musculos_treino" ADD CONSTRAINT "musculos_treino_treino_id_treinos_ext_id_fk" FOREIGN KEY ("treino_id") REFERENCES "public"."treinos"("ext_id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "treinos" ADD CONSTRAINT "treinos_usuario_id_users_table_ext_id_fk" FOREIGN KEY ("usuario_id") REFERENCES "public"."users_table"("ext_id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
---
+++ src/database/migrations/0001_overrated_wrecking_crew.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "users_table" ALTER COLUMN "street" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "users_table" ALTER COLUMN "street_number" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "users_table" ALTER COLUMN "neighborhood" DROP NOT NULL;
